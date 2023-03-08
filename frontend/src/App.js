316e443cfeb1bf4e2d5cb69d54134dda2fe6b695
Jason Huang <jason97521@gmail.com>
2023-03-08 08:37:52 -0500

    mapbox pop up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,11 @@
 import * as React from 'react';
-import ReactMapGL, {Marker} from 'react-map-gl'
+import ReactMapGL, {Marker, Popup} from 'react-map-gl'
+import {Room, Star} from '@mui/icons-material';
+import "./app.css"
 
 
 function App() {
+  const [showPopup, setShowPopup] = React.useState(true);
   return <ReactMapGL
     initialViewState={{
       latitude: 48.858093,
@@ -14,8 +17,30 @@ function App() {
     mapboxAccessToken={process.env.REACT_APP_MAPBOX}
     >
       <Marker longitude={2.294694} latitude={48.858093} anchor="bottom" >
-        <div>YOU ARE HERE</div>
+        <Room style={{color:"slateblue" }} />
       </Marker>
+      {/* {showPopup && (
+      <Popup longitude={2.294694} latitude={48.858093}
+        anchor="bottom"
+        onClose={() => setShowPopup(false)}>
+        <div className='card'>
+          <label>Place</label>
+          <h4 className='place'>Eiffell Tower</h4>
+          <label>Review</label>
+          <p className='desc'>Beautiful place. I like it.</p>
+          <label>Rating</label>
+          <div className='stars'>
+            <Star className='star'/>
+            <Star className='star'/>
+            <Star className='star'/>
+            <Star className='star'/>
+            <Star className='star'/>
+          </div>
+          <label>Information</label>
+          <span className='username'>Created by <b>jason</b></span>
+          <span className='date'>1 hour ago </span>
+        </div>
+      </Popup>)} */}
   </ReactMapGL>
 }
 

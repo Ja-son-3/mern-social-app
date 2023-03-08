815d516dcd9baa04587af665d763e0b5cfa6cd54
Jason Huang <jason97521@gmail.com>
2023-03-08 05:44:19 -0500

    node.js login and register

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,7 +2,6 @@ const router = require("express").Router()
 const User = require("../models/User")
 const bcrypt = require("bcrypt")
 
-
 //register
 
 router.post("/register", async (req,res)=>{
@@ -26,7 +25,26 @@ router.post("/register", async (req,res)=>{
     }
 })
 
-
 //login
+
+router.post("/login", async (req,res)=>{
+    try{
+        //find user
+        const user = await User.findOne({ username: req.body.username })
+        !user && res.status(400).json("Wrong username or password!")
+
+        //validate password
+        const validPassword = await bcrypt.compare(
+            req.body.password,
+            user.password
+        )
+        !validPassword && res.status(400).json("Wrong username or password!")
+
+        //send successful response
+        res.status(200).json({_id:user._id, username:user.username})
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
 
 module.exports = router

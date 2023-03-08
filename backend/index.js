9a758f8ec020beed3dcbda8caad24851c5386dd8
Jason Huang <jason97521@gmail.com>
2023-03-08 09:24:47 -0500

    axios, add location

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,25 @@
-const express = require("express")
-const mongoose = require("mongoose")
-const dotenv = require("dotenv")
-const app = express()
-const userRoute = require("./routes/users")
-const pinRoute = require("./routes/pins")
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/users");
+const pinRoute = require("./routes/pins");
 
-dotenv.config()
+dotenv.config();
 
-app.use(express.json())
+app.use(express.json());
 
-mongoose
-    .connect(process.env.MONGO_URL, { 
+mongoose 
+ .connect(process.env.MONGO_URL, {
         useNewUrlParser: true,
-        useUnifiedTopology:true,
-    })
-    .then(()=>{
-        console.log("MongoDB Connected")
-    })
-    .catch((err)=> console.log(err))
+        useUnifiedTopology: true,
+        useCreateIndex: true,   })   
+ .then(() => console.log("MongoDB connected!"))
+ .catch(err => console.log(err));
 
-    app.use("/api/users", userRoute)
-    app.use("/api/pins", pinRoute)
+app.use("/api/users", userRoute);
+app.use("/api/pins", pinRoute);
 
-app.listen(8800, ()=>{
-    console.log("Backend server is running!")
-})
+app.listen(8800, () => {
+  console.log("Backend server is running!");
+});
